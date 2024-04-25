bd07b0bbe1ca2204e53cda1e1c7f8b40c5832d71
Abu Bakar <2010ee146@gmail.com>
2024-04-25 09:51:35 +0500

    Add files via upload

--- Logout.py
+++ Logout.py
@@ -4,6 +4,12 @@ import hmac
 import streamlit as st
 
 # set sidebar collapsed before login
+#list of keys tied to widgets that you want to protect
+keeper_list = ['username', "user"]
+
+for key in keeper_list:
+    if key in st.session_state:
+        st.session_state[key] = st.session_state[key]
 def collapse():
     st.markdown("""
         <style>
@@ -22,8 +28,8 @@ def check_password():
     def login_form():
         """Form with widgets to collect user information"""
         with st.form("Credentials"):
+            st.session_state
             st.text_input("Username", key="username")
-            st.session_state.user = st.session_state["username"]
             st.text_input("Password", type="password", key="password")
             st.form_submit_button("Log in", on_click=password_entered)
 
@@ -37,7 +43,8 @@ def check_password():
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
+            st.session_state.user = st.session_state["username"]
+            #del st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
             st.session_state["login_status"] = False
@@ -51,8 +58,12 @@ def check_password():
     if "password_correct" in st.session_state:
         st.error("😕 User not known or password incorrect")
     return False
+
+
 def clickedLogout():
     st.session_state["password_correct"] = False
+    st.session_state.user = ""
+    st.session_state["username"] = ""
     collapse()
     if not check_password():
         st.stop()
@@ -61,7 +72,7 @@ def clickedLogout():
 if not check_password():
     st.stop()
 
-
+st.session_state
 st.button("Logout" , on_click=clickedLogout)
     
 
